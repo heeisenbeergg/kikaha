e8650e7e7b8da7dd3d88144cb52d43b40df02694
Miere Teixeira <miere00@gmail.com>
2019-03-20 23:06:39 +1100

    Initial documentation for Rest API annotations.

--- kikaha-rest-api/source/kikaha/rest/api/HeaderParam.java
+++ kikaha-rest-api/source/kikaha/rest/api/HeaderParam.java
@@ -15,4 +15,6 @@ import java.lang.annotation.Target;
 public @interface HeaderParam {
 
     String value();
+
+    boolean required() default false;
 }
--- kikaha-rest-api/source/kikaha/rest/api/PathParam.java
+++ kikaha-rest-api/source/kikaha/rest/api/PathParam.java
@@ -15,4 +15,6 @@ import java.lang.annotation.Target;
 public @interface PathParam {
 
     String value();
+
+    boolean required() default false;
 }
